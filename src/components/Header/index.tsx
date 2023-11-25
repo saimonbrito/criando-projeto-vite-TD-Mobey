9d57d216d809bc62969be1b1e8dfe860cf58349e
saimonBrito <britomauricio89@gmail.com>
2023-11-25 14:51:55 -0300

    Sumsricards

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { HeaderContainer, HeaderContent, NewTransitionButton } from "./styles";
+import logoImg from '../../assets/logo.svg'
+
+export function Header(){
+  return(
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoImg} alt="" />
+        <NewTransitionButton>Nova trasação</NewTransitionButton>
+      </HeaderContent>
+    </HeaderContainer>
+  )
+}
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,22 +2,23 @@ import { createGlobalStyle } from "styled-components";
 
 
 export const GlobalStyle = createGlobalStyle`
-  *{
-    margin:0;
-    paddng: 0;
-    box-sizing:borde-box;
-  }
-  :focus{
-    cutline:0;
-    box-shadow: 0 0 0 2px ${props => props.theme['green-500']}
-  }
-  body {
-    background-color:${props => props.theme['gray-800']}
-    color:${props => props.theme['gray-100']}
-    -webkit-font-smoothing: antiaLiased;
-  }
-  body ,input ,testarea, button {
-    font:400 1rem Roboto, sans-serif;
-  }
+    *{
+      margin:0;
+      padding: 0;
+      box-sizing:borde-box;
+    }
+    :focus{
+      outline:0;
+      box-shadow: 0 0 0 2px ${props => props.theme['green-500']};
+    }
+    body {
+      background-color:${props => props.theme['gray-800']};
+      color:${props => props.theme['gray-100']};
+      -webkit-font-smoothing: antiaLiased;
+    }
+    body ,input ,testarea, button {
+      font:400 1rem Roboto, sans-serif;
+    }
+
 
 `;
